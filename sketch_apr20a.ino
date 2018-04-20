502acd098af3b6c3578b0f2ddaa1b97fb56dbaa4
arturogu <arturogu95@gmail.com>
2018-04-20 14:52:13 -0600

    Update sketch_apr20a.ino

--- sketch_apr20a.ino
+++ sketch_apr20a.ino
@@ -1,19 +1,24 @@
 int ledPin = 5;
 int buttonPin = 6;
+int ButtonValue = 0;
  
 void setup()
 {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
  
 void loop()
 {
   // read from the button pin
-  int button = digitalRead(buttonPin);
-  if (button==HIGH)
+  ButtonValue = digitalRead(buttonPin);
+  if (ButtonValue!=0)
   {
     digitalWrite(ledPin,HIGH);
+    Serial.write(1);
+    Serial.flush();
+    delay(2000);
   }
   else
   {
