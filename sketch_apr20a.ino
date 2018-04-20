a34a2229aabd47a9c4b7de71c86f7dd6cb0b299d
arturogu <arturogu95@gmail.com>
2018-04-20 10:28:30 -0600

    Add files via upload

---
+++ sketch_apr20a.ino
@@ -0,0 +1,22 @@
+int ledPin = 5;
+int buttonPin = 6;
+ 
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+ 
+void loop()
+{
+  // read from the button pin
+  int button = digitalRead(buttonPin);
+  if (button==HIGH)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
